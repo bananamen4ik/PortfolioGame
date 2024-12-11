99ea4bebafed7f3e9163da2b520469bf9530b42e
bananamen4ik <bananamen4ik@icloud.com>
2024-12-11 21:19:36 +0300

    Added new Input System package and set it up for basic move. Added Character Controller component for player character and write script for basic move without mouse. Removed Box collider and Rigidbody from player character. Added Player Input component for player character

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector2 moving = new();
+    private float speedMove = 1;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 move = new(moving.x, 0, moving.y);
+
+        if (move == Vector3.zero) return;
+
+        characterController.Move(speedMove * Time.deltaTime * move);
+        transform.forward = move;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moving = value.Get<Vector2>();
+    }
+}
