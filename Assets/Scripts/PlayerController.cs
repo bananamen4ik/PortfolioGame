cb140ca08e69c3744d23b2f1f2182c242a019ac0
bananamen4ik <bananamen4ik@icloud.com>
2024-12-12 19:09:01 +0300

    Added cinemachine package. Added cinemachine virtualcamera for character and main camera with cinemachine brain component. Added mouse camera control, proper keyboard movement control and walking animation playback

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,21 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject mainCamera;
+
     private CharacterController characterController;
+    private Animator characterAnimator;
     private Vector2 moving = new();
-    private float speedMove = 1;
+    private readonly float speedMove = 2;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        characterAnimator = GetComponent<Animator>();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
@@ -20,15 +28,31 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         Vector3 move = new(moving.x, 0, moving.y);
-
         if (move == Vector3.zero) return;
 
-        characterController.Move(speedMove * Time.deltaTime * move);
-        transform.forward = move;
+        Vector3 cameraForward = mainCamera.transform.forward;
+        Vector3 cameraRight = mainCamera.transform.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        Vector3 direction = move.x * cameraRight + move.z * cameraForward;
+
+        transform.forward = direction;
+        characterController.Move(speedMove * Time.deltaTime * direction);
     }
 
     public void OnMove(InputValue value)
     {
         moving = value.Get<Vector2>();
+
+        if (moving != Vector2.zero)
+        {
+            characterAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            characterAnimator.SetBool("IsWalking", false);
+        }
     }
 }
